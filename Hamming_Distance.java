6ffab6b4061944bd97cc9d8077f8435e42cd18a8
Balaji Chandrasekaran <bchandr6@asu.edu>
2017-02-26 03:00:45 -0700

    Hamming_Distance.
    The Hamming distance between two integers is the number of positions at
    which the corresponding bits are different.
    
    Given two integers x and y, calculate the Hamming distance.

---
+++ Hamming_Distance.java
@@ -0,0 +1,14 @@
+public class Solution 
+{
+    public int hammingDistance(int x, int y) 
+    {
+        int z = (int) (x^y);
+        int count = 0;
+        while(z != 0)
+        {
+            z &= (z-1);
+            count++;
+        }
+        return count;
+    }
+}
