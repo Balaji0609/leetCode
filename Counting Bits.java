e78082a1ca734c7bf02328978836876422aa50e4
Balaji Chandrasekaran <bchandr6@asu.edu>
2017-02-26 02:58:11 -0700

    Documentation.
    Added documentation.

--- Counting Bits.java
+++ Counting Bits.java
@@ -1,11 +1,23 @@
-
+/**
+ * This class contains the fucntion to count the bits.  
+ *
+ * @author      Balaji Chandrasekaran.
+ * @version     1.0
+ *
+ */
 public class Solution 
 {
+	/**
+     * This is the function which takes num as input and generates the count of all the bits set from 1 to the given num.
+     *
+     * @param num    	Upto which the count of set bits need to be found.    
+     * @return int[]    Return an array of counts of set bit for each num from 1.      
+     */
     public int[] countBits(int num) 
     {
         int[] retarr = new int[num+1];
         retarr[0] = 0;
-        for(int i = 1 ; i <= num; i++)
+        for(int i = 1 ; i <= num; i++)				// Only runs n times.
         {
             retarr[i] = retarr[i&(i-1)] + 1; 
         }
