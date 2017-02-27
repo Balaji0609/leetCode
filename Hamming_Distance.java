6c3c69c999f64a974b6fd955b62cb7ae1941539f
Balaji Chandrasekaran <bchandr6@asu.edu>
2017-02-27 00:18:09 -0700

    Comment.
    Added documentational comment.

--- Hamming_Distance.java
+++ Hamming_Distance.java
@@ -1,5 +1,19 @@
+/**
+ * This class contains the fucntion to find the Hamming Distance.  
+ *
+ * @author      Balaji Chandrasekaran.
+ * @version     1.0
+ *
+ */
 public class Solution 
 {
+	/**
+     * This is the function which takes two number x and y as input and generates the Hamming Distance of them.
+     *
+     * @param x   	Input 1 to find the Hamming Distance.
+	 * @param y		Input 2 to find the Hamming Distance.
+     * @return int    Return the hamming distance of two numbers.      
+     */
     public int hammingDistance(int x, int y) 
     {
         int z = (int) (x^y);
