588c2c3eefa3d9e2882a7a615723d49a407fe295
Balaji0609 <Balaji0609@users.noreply.github.com>
2017-07-25 22:28:25 -0700

    New program

---
+++ leetCode/src/leetCode/Maximum_Depth_of_Binary_Tree_104.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+public class Maximum_Depth_of_Binary_Tree_104 
+{
+	public class TreeNode 
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	public int maxDepth(TreeNode root) 
+	{
+	    if(root == null)
+	        return 0;
+	    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+	}
+}
