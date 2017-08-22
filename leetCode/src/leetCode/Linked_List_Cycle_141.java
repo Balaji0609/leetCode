62201d1f252f6220f6d7fdf8000e7d1902b67089
Balaji0609 <Balaji0609@users.noreply.github.com>
2017-08-22 14:55:00 -0700

    New Code 141

---
+++ leetCode/src/leetCode/Linked_List_Cycle_141.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+public class Linked_List_Cycle_141 
+{
+	 class ListNode {
+		 int val;
+		 	ListNode next;
+		 	ListNode(int x) {
+			 val = x;
+			 next = null;
+		 }
+	 }
+    public boolean hasCycle(ListNode head) 
+    {
+        if(head == null || head.next == null)
+            return false;
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while(fast != slow)
+        {
+            if(fast == null || fast.next == null)
+                return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
