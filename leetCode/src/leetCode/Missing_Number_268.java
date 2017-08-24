2c8970aa5f634064bee6cea6dda86b538d298f17
Balaji0609 <Balaji0609@users.noreply.github.com>
2017-08-24 14:24:29 -0700

    New Program 268

---
+++ leetCode/src/leetCode/Missing_Number_268.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+public class Missing_Number_268 
+{
+    public int missingNumber(int[] nums) 
+    {
+        int res = nums.length;
+        for(int i = 0; i < nums.length; i++)
+        {
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
