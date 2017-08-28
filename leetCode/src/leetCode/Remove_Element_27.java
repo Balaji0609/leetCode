efd6899662ede97def9da7b993960a8d23f30f9c
Balaji0609 <Balaji0609@users.noreply.github.com>
2017-08-27 21:38:52 -0700

    New Program 27

---
+++ leetCode/src/leetCode/Remove_Element_27.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+public class Remove_Element_27 
+{
+    public int removeElement(int[] nums, int val) 
+    {
+        int start = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] != val)
+                nums[start++] = nums[i];
+        }
+        return start;
+    }
+}
