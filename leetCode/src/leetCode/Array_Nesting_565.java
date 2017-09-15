6da9efcc6e00e6f6e49341131c31bc51ec0b1515
Balaji0609 <Balaji0609@users.noreply.github.com>
2017-09-14 18:55:26 -0700

    New Problem

---
+++ leetCode/src/leetCode/Array_Nesting_565.java
@@ -0,0 +1,26 @@
+package leetCode;
+
+public class Array_Nesting_565 
+{
+    public int arrayNesting(int[] nums) 
+    {
+        int size = 0;
+        for(int i = 0 ; i < nums.length;i++)
+        {
+            if(nums[i] == -1)
+                continue;
+            int j = i;
+            int currSize = 0;
+            while(nums[j]>=0)
+            {
+                int temp = nums[j];
+                nums[j] = -1;
+                currSize++;
+                j = temp;
+            }
+            size = Math.max(currSize,size);
+        }
+        return size;
+    }
+
+}
